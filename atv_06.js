55b7e275ef9056f7ac5a4b9f32f21c10e7024420
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-04-25 10:28:55 -0300

    Mostra os itens de um array de trás pra frente

---
+++ atv_06.js
@@ -0,0 +1,7 @@
+let lista = ["Yakisoba", "Lasanha de berinjela", "Tacos mexicanos", "Feijoada", "Sopa de cebola"]
+
+itens = lista.length
+
+for (let i = (itens - 1); i >= 0; i--) {
+    console.log(lista[i])
+}
