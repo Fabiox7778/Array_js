f8108b1aeda6b58af267ae3266e668cc5a24624b
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-04-25 11:51:37 -0300

    3 jogos representando um ranking.

---
+++ atv_10.js
@@ -0,0 +1,23 @@
+//definindo as variaveis
+let jogos = ["The Last of Us", "Red Dead Redemption 2", "Elden Ring"]
+let favGame = "The Last of Us"
+let jogo = "Elden ring"
+
+//definindo quantos jogos tem
+let quant = jogos.length
+
+//definindo o ranking
+for (let i = 0; i < quant; i++) {
+    console.log(`O jogo ${jogos[i]} está em ${i + 1} posição`)
+}
+
+console.log("--------------------------")
+
+//decidindo se é o favorito
+if (jogo === favGame) {
+    console.log(`Sim, ${jogo} é o favorito👍`)
+} else {
+    console.log(`Não, ${jogo} não é o favorito👎 ${i+1}`)
+}
+
+console.log("--------------------------")
