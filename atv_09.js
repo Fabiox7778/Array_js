a4792aa5e4c114604abd1dd08c6d3d402a0da178
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-04-25 10:59:26 -0300

    lista de 6 frutas que mostra quantas tem no total

---
+++ atv_09.js
@@ -0,0 +1,8 @@
+let frutas = ["Abacaxi", "Uva", "Maça", "Banana", "Kiwi", "Melancia"]
+let quant = frutas.length
+
+console.log(`Aqui temos ${quant} tipos de fruta no estoque`)
+console.log("⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇")
+for (let i = 0; i < quant; i++) {
+    console.log(frutas[i])
+}
