3853ffbd11ffd72602607f517e3687d90095cfd6
Fabio Trevizolli <fabiorabello08@gmail.com>
2025-04-25 09:36:59 -0300

    4 músicas que gosto com for

---
+++ atv_02.js
@@ -0,0 +1,7 @@
+let musicas = ["Beautiful Day-U2", "Coraçao na Geladeira-Kevin", "Amiga da minha mulher-Seu Jorge", "Fim de Semana no Parque-Racionais"]
+
+let quantidade = (musicas.length)
+
+for (let i = 0; i < quantidade; i++) {
+    console.log(musicas[i])
+}
